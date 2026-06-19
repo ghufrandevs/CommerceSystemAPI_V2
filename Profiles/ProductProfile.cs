76b5b255e02b9a3b04f99db7261699c04e28013b
ghufran <22ghufran2002@gmail.com>
2026-06-19 16:26:03 +0400

    Add Product AutoMapper profiles

---
+++ Profiles/ProductProfile.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using CommerceSystemAPI.DTOs;
+using CommerceSystemAPI.Models;
+
+namespace CommerceSystemAPI.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<ProductCreateDTO, Product>();
+
+            CreateMap<ProductUpdateDTO, Product>();
+
+            // Custom Mapping because CategoryName and SupplierName
+            // are inside navigation properties, not directly in Product.
+            CreateMap<Product, ProductOutputDTO>()
+                .ForMember(
+                    dest => dest.CategoryName,
+                    opt => opt.MapFrom(src =>
+                        src.Category != null
+                            ? src.Category.CategoryName
+                            : null))
+
+                .ForMember(
+                    dest => dest.SupplierName,
+                    opt => opt.MapFrom(src =>
+                        src.Supplier != null
+                            ? src.Supplier.SupplierName
+                            : null));
+        }
+    }
+}
